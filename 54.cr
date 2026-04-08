f61a8485984a3212f58fa58c1a233e14e9cab74f
RetiredGuitar64 <retiredguitar64@qq.com>
2026-04-08 21:45:41 +0800

    2026-04-08

--- 54.cr
+++ 54.cr
@@ -5,3 +5,4 @@ def invoke(&block) # 方法要求接收一个block
 end
 
 invoke(&p1) # 这里需要加 &p1, 把p1这个proc变成block, 再传给invoke方法, 不加的话,会报错 Error: 'invoke' is expected to be invoked with a block, but no block was given
+
